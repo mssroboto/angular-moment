3a0b0a299244a07822c09fb7504956e7acc53da9
Samanta Martinez <lsmartinezru@gmail.com>
2020-05-19 17:02:22 -0500

    [DEVTASK]::Add project foundation.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit {
+
+  constructor() {}
+
+  time: string = moment().format()
+
+  ngOnInit(): void {
+  }
+}
